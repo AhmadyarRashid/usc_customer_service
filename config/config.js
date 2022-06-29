33c6b31adbd7f50c1997814767a3d5e424f85966
AhmadyarRashid <ahmadyarmeo@gmail.com>
2022-06-29 14:31:25 +0500

    update password

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   db_port: "3306",
   db_name: "CustomerService",
   db_user: "root",
-  db_pass: "root",
+  db_pass: "Pakistan786@",
   db_poolsize: "100",
   server_port: "3003",
   jwt_secret: "AEE9D18BCC524554BFD7A59EA088EC7D424039CDBBFFBFF050ADE6BC4F8A8F83"
